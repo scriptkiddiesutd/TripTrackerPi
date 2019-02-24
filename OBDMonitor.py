2454ed9b11cf8e5430e74c826249215f48577b0c
Lathe Abusaid <labusaid@gmail.com>
2019-02-23 18:27:28 -0600

    created basic querying stuff

--- OBDMonitor.py
+++ OBDMonitor.py
@@ -3,11 +3,12 @@ import time
 
 
 class InstantData:
-	def __init__(self, seconds, currentFuelLevel, currentFuelRate, currentSpeed):
+	def __init__(self, seconds, currentFuelLevel, currentFuelRate, currentSpeed, currentMPG):
 		self.elapsedSeconds = seconds
 		self.fuelLevel = currentFuelLevel
 		self.fuelRateGPH = currentFuelRate
 		self.vehicleSpeed = currentSpeed
+		self.instantMPG = currentMPG
 
 
 # obd connection setup
@@ -18,18 +19,21 @@ cmd = obd.commands.SPEED  # select an OBD command (sensor)
 querySpeed = 5
 
 vehicleData = []
+currentIndex = 0
 elapsedSeconds = 0
 
 while True:
-	elapsedSeconds += querySpeed
 
-	vehicleData.append(InstantData(elapsedSeconds, connection.query("FUEL_LEVEL"), connection.query("FUEL_RATE"), connection.query("SPEED")))
+	vehicleData.insert(currentIndex, InstantData(elapsedSeconds, connection.query("FUEL_LEVEL"), connection.query("FUEL_RATE"), connection.query("SPEED")))
 
 	# print data
-	print("elapsedSeconds: " + vehicleData[elapsedSeconds].fuelLevel)
-	print("fuelRateLPH: " + vehicleData[elapsedSeconds].fuelLevel)
-	# print("fuelRateGPH: " + vehicleData[elapsedSeconds].fuelLevel.value.to("gph"))
-	print("fuelLevel: " + vehicleData[elapsedSeconds].fuelLevel)
+	print("elapsedSeconds: " + vehicleData[currentIndex].fuelLevel)
+	print("fuelRateLPH: " + vehicleData[currentIndex].fuelLevel)
+	print("fuelRateGPH: " + vehicleData[currentIndex].fuelLevel.value.to("gph"))
+	print("fuelLevel: " + vehicleData[currentIndex].fuelLevel)
+
+	elapsedSeconds += querySpeed
+	currentIndex += 1
 
 	# delay
 	time.sleep(querySpeed)
