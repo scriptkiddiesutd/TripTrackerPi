b88d05c4634af0906f3fd4ef15fde65f42225e24
Lathe Abusaid <labusaid@gmail.com>
2019-02-23 18:11:24 -0600

    created basic querying stuff

--- OBDMonitor.py
+++ OBDMonitor.py
@@ -1,35 +1,35 @@
 import obd
 import time
 
-#class InstantData:
 
+class InstantData:
+	def __init__(self, seconds, currentFuelLevel, currentFuelRate, currentSpeed):
+		self.elapsedSeconds = seconds
+		self.fuelLevel = currentFuelLevel
+		self.fuelRateGPH = currentFuelRate
+		self.vehicleSpeed = currentSpeed
 
 
 # obd connection setup
 connection = obd.OBD()  # auto-connects to USB or RF port
 cmd = obd.commands.SPEED  # select an OBD command (sensor)
 
-# sets the speed
+# sets the amount of seconds between queries
 querySpeed = 5
 
+vehicleData = []
 elapsedSeconds = 0
-distanceTraveled = 0
-#vehicleData = []
 
 while True:
-    elapsedSeconds += querySpeed
+	elapsedSeconds += querySpeed
 
-    # query vehicle for data
-    fuelLevel = connection.query("FUEL_LEVEL")
-    fuelRateLPH = connection.query("FUEL_RATE")
-    vehicleSpeed = connection.query("SPEED")
+	vehicleData.append(InstantData(elapsedSeconds, connection.query("FUEL_LEVEL"), connection.query("FUEL_RATE"), connection.query("SPEED")))
 
-    # print data
-    print(elapsedSeconds)
-    print(fuelLevel.value)  # returns unit-bearing values thanks to Pint
-    print(fuelRateLPH.value.to("gph"))  # user-friendly unit conversions
+	# print data
+	print("elapsedSeconds: " + vehicleData[elapsedSeconds].fuelLevel)
+	print("fuelRateLPH: " + vehicleData[elapsedSeconds].fuelLevel)
+	# print("fuelRateGPH: " + vehicleData[elapsedSeconds].fuelLevel.value.to("gph"))
+	print("fuelLevel: " + vehicleData[elapsedSeconds].fuelLevel)
 
-    # write data to list
-
-    # delay
-    time.sleep(querySpeed)
+	# delay
+	time.sleep(querySpeed)
